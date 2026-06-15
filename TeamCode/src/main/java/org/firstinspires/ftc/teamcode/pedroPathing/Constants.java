c7b2d42103b3291a3b177d6d89f7400c2494cc1e
Vulcu <razvanioanvulcu@gmail.com>
2026-06-15 10:53:54 +0300

    autonomy test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,48 +1,54 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
 
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(13.3)
+            .forwardZeroPowerAcceleration(-41.82)
+            .lateralZeroPowerAcceleration(-75.0204)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.09,0.0,0.002,0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(1,0.0,0.001,0.02));
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1.0)
-
-            // CHANGE THESE ONLY IF YOUR CONFIG NAMES ARE DIFFERENT
+            .maxPower(1)
             .rightFrontMotorName("RF")
             .rightRearMotorName("RB")
-            .leftFrontMotorName("LF")
             .leftRearMotorName("LB")
-
-            // Usually one side is reversed on mecanum
+            .leftFrontMotorName("LF")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+
+            .xVelocity(76.38628)
+            .yVelocity(67.723279);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            // Your remembered Tulumba Power Pinpoint offsets, in inches
-            .forwardPodY(5.04)
-            .strafePodX(5.83)
-
-            // You may need to flip one of these after testing
+            .forwardPodY(-4.5)
+            .strafePodX(-6)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
-    public static PathConstraints pathConstraints = new PathConstraints(
-            0.99,
-            100,
-            1,
-            1
-    );
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100,
+            0.9,
+            1.1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
